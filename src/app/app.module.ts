fd76c1683c35998439dc2c3d032b8e566dd79cd7
mregger <eduardoegger@gmail.com>
2020-06-03 16:41:03 -0300

    feat(http) get data from backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,9 +20,10 @@ import { ApiService } from './services/api.service';
     HorrorscopeDisplayComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
 
@@ -7,11 +8,10 @@ import { Observable, of } from 'rxjs';
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
 
   public getHorror(sign: string): Observable<string> {
-    let c = 'balls'
-    let b = 'boobs'
-    return sign === 'capricorn' ? of(b) : of(c);
+    const url: string = 'https://us-central1-my-horrorscopes.cloudfunctions.net/get-data?sign=' + sign;
+    return this._http.get<string>(url);
   }
 }
