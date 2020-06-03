bb8cbe2b728e701cf6f1a3fff82509ffffa71d23
mregger <eduardoegger@gmail.com>
2020-06-03 16:50:20 -0300

    feat(config) proxy results to gcloud function

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -11,7 +11,7 @@ export class ApiService {
   constructor(private _http: HttpClient) { }
 
   public getHorror(sign: string): Observable<string> {
-    const url: string = 'https://us-central1-my-horrorscopes.cloudfunctions.net/get-data?sign=' + sign;
+    const url: string = '/api/get-data?sign=' + sign;
     return this._http.get<string>(url);
   }
 }
